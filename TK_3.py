f9cafddcab751a040d2bc5ea7b23aaa6b6002ee2
Oleg <Kabachenko.Ol.V.nmu.one>
2022-10-10 14:31:33 +0300

    TK3. Ad get_average_list() function

---
+++ TK_3.py
@@ -0,0 +1,15 @@
+def get_average_list(list_data):
+    new_list = []
+    average = float(sum(list_data)) / len(list_data)
+
+    if len(list_data) > 0:
+
+        i = 0
+        while i < len(list_data):
+            new_list += [float(list_data[i]) / float(average)]
+            i += 1
+
+        return new_list
+    else:
+        return 0
+
